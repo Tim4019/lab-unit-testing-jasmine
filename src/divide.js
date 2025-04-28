10044334105271f52097c6e42f38dd9e18e5485f
Timal Silva <tim@MacBookPro.home>
2025-04-28 18:01:09 +0200

    lab solved

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,11 @@
-function divide () {
-       
+function divide(numOne, numTwo) {
+    if (typeof numOne !== 'number' || typeof numTwo !== 'number') {
+        return undefined;
+    }
+
+    if (numTwo === 0) {
+        return undefined;
+    }
+
+    return numOne / numTwo;
 }
